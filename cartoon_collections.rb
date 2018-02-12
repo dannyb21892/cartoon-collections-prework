d0ca6f178bceddc21eba099bc1243621622f43c8
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-12 16:08:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
