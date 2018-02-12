bbf2124c2117f5eb14c865749ae67a62cda76a98
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-12 16:11:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ def long_planeteer_calls(words)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(input)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.any? do |cheese|
+    
+  end
 end
